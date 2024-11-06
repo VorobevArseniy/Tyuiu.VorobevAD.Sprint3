c3e0f41f148a3c4d23cbff8d675fba70282b5c78
uiuiuiui15 <dr.venitus@gmail.com>
2024-11-06 22:27:22 +0500

    Add project files.

---
+++ Tyuiu.VorobevAD.Sprint3.Task0.V19.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.VorobevAD.Sprint3.Task0.V19.Lib
+{
+    public class DataService : tyuiu.cources.programming.interfaces.Sprint3.ISprint3Task0V19
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+            double S = 0;
+
+            for (int i = startValue; i < stopValue; ++i)
+            {
+                S = Math.Pow((1 / (Math.Cos(i) + 2)), 2);
+            }
+            return S;
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.VorobevAD.Sprint3.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.VorobevAD.Sprint3.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
