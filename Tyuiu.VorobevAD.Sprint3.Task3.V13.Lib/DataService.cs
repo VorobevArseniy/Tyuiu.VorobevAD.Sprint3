b2dc37fc1611fd0bba2fc7488cc0dc2b29d39651
uiuiuiui15 <dr.venitus@gmail.com>
2024-11-14 16:32:41 +0500

    a

---
+++ Tyuiu.VorobevAD.Sprint3.Task3.V13.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.VorobevAD.Sprint3.Task3.V13.Lib
+{
+    public class DataService : tyuiu.cources.programming.interfaces.Sprint3.ISprint3Task3V13
+    {
+        public int ConvertStringToInt(string value)
+        {
+            string s = "";
+
+            foreach (char i in value)
+            {
+                if (i == '5')
+                {
+                    s = "5";
+                }
+            }
+
+            return ConvertStringToInt(s);
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.VorobevAD.Sprint3.Task3.V13.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+
+
+namespace Tyuiu.VorobevAD.Sprint3.Task3.V13.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
