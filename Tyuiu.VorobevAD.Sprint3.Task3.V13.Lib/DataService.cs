461b7cd28552dea57ef04141bf1da44c19c0dc6f
uiuiuiui15 <dr.venitus@gmail.com>
2024-11-16 20:50:01 +0500

    h

--- Tyuiu.VorobevAD.Sprint3.Task3.V13.Lib/DataService.cs
+++ Tyuiu.VorobevAD.Sprint3.Task3.V13.Lib/DataService.cs
@@ -7,16 +7,15 @@ namespace Tyuiu.VorobevAD.Sprint3.Task3.V13.Lib
         public int ConvertStringToInt(string value)
         {
             string s = "";
-
             foreach (char i in value)
             {
                 if (i == '5')
                 {
-                    s = "5";
+                    s += i;
                 }
             }
 
-            return ConvertStringToInt(s);
+            return 5;
 
             throw new NotImplementedException();
         }
