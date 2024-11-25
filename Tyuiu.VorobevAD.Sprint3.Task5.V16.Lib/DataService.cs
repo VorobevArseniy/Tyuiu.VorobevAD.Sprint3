43ec9755c4ccad70aaec1e164168c668d8fb033a
uiuiuiui15 <dr.venitus@gmail.com>
2024-11-25 22:31:10 +0500

    a

---
+++ Tyuiu.VorobevAD.Sprint3.Task5.V16.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.VorobevAD.Sprint3.Task5.V16.Lib;
+
+public class DataService : tyuiu.cources.programming.interfaces.Sprint3.ISprint3Task5V16
+{
+	public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+	{
+
+		double y = 0;
+
+		for (int i = startValue1; i <= stopValue1; i++)
+		{
+			for (int k = startValue2; k <= stopValue2; k++)
+			{
+				y += Math.Cos(k);
+			}
+		}
+
+		y += Math.Pow(x, 2);
+
+		return Math.Round(y, 3);
+		throw new NotImplementedException();
+	}
+}
